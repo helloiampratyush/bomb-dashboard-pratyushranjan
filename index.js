c9eb9a1478a3804924eebd6a3eef212c979d1692
pratyush ranjan <82261854+PRATYUSHRANJAN060502@users.noreply.github.com>
2023-02-10 13:30:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+ function fun1(){
+    window.open("https://www.bomb.farm/#/bomb")
+    }
+    function fun2(){
+    window.open("https://app.bogged.finance/bsc/swap?tokenIn=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&tokenOut=0x522348779DCb2911539e76A1042aA922F9C47Ee3")
+    }
+    function fun3(){
+    window.open("https://app.bogged.finance/bsc/swap?tokenIn=BNB&tokenOut")
+    }
+    function fun4(){
+    window.open("https://app.bogged.finance/bsc/swap?tokenIn=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&tokenOut=0x6216B17f696B14701E17BCB24Ec14430261Be94A")
+    }
+    function fun5(){
+    window.open("https://dexscreener.com/bsc/0x84392649eb0bc1c1532f2180e58bae4e1dabd8d6")
+    }
+    function fun6(){
+    window.open("https://dexscreener.com/bsc/0x1303246855b5b5ebc71f049fdb607494e97218f8")
+    }
+    function fun7(){
+    window.open("https://www.youtube.com/watch?v=nhCWmmRNNhc&t=22s")
+    }
